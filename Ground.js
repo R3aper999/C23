938764d9d6d435ae13db39b2a0e6241da5a10e1f
R3aper999 <79029287+R3aper999@users.noreply.github.com>
2021-04-04 20:58:12 -0400

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor(x,y,height){
+
+    var options = {
+        isStatic:true
+}
+this.body = Bodies.rectangle(x,y,400,height,options);
+this.width = 400;
+this.height = height;
+World.add(world,this.body)
+}
+display(){
+var pos = this.body.position
+rectMode(CENTER)
+rect(pos.x,pos.y,this.width,this.height)
+}
+}
